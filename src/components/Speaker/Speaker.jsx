80e4bfed2589f425d6ac68ff0d43d8e46c35805a
codebuilder2022 <kumarankaushik@gmail.com>
2022-02-08 15:03:23 +0530

    fix: UI fixes

--- src/components/Speaker/Speaker.jsx
+++ src/components/Speaker/Speaker.jsx
@@ -1,36 +1,76 @@
-import React from 'react';
+import React from "react";
 
-import styled from 'styled-components';
-import bg from '../../assets/overlay.png'
+import "../Judges/judges.css";
+import styled from "styled-components";
+import bg from "../../assets/overlay.png";
+
+const SPEAKER = [
+  {
+    name: "Cool Guy",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Cool Guy",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Cool Guy",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+];
 
 const Heading = styled.h2`
-text-align: center;
-font-size: 65px;
-color: rgba(255, 255, 255, 0.95);
-margin-bottom: 130px;
-margin-top: 100px;
-font-weight: 800;
-text-decoration-color: 2px solid #deb7f5;
-text-shadow: 0px 0px 50px #b741ff;
-  
+  text-align: center;
+  font-size: 65px;
+  color: rgba(255, 255, 255, 0.95);
+  margin-bottom: 50px;
+  margin-top: 20px;
+  font-weight: 800;
+  text-decoration-color: 2px solid #deb7f5;
+  text-shadow: 0px 0px 50px #b741ff;
 `;
-const myStyle={
-  backgroundImage:`url(${bg})`,
-  height:'100vh',
-  marginTop:'-70px',
-  fontSize:'50px',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
+const myStyle = {
+  backgroundImage: `url(${bg})`,
+  height: "100%",
+  fontSize: "50px",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
 };
-const Speaker = () => {
+
+const Mentor = () => {
   return (
-    <div style={myStyle}> 
-      <Heading>Speakers</Heading>
-      
-      
-     
+    <div style={myStyle}>
+      <Heading>Speaker</Heading>
+      <div className="row gx-0 py-5 justify-content-center">
+        {SPEAKER.map((item, index) => {
+          return (
+            <div className="col-12 col-lg-3 mb-15 speaker">
+              <div className="speakers">
+                <div className="speaker-img">
+                  <img src={`${item.image}`} alt={`${item.name}`} />
+                </div>
+                <div className="speaker-details ">
+                  <div className="speaker-name">{item.name}</div>
+                  <div className="speaker-desg">
+                    {item.designation}, {item.company}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default Speaker;
+export default Mentor;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import Landing from "../components/Home/Landing";
 import styled from "styled-components";
 
-import Speaker from '../components/Speaker/Speaker';
-import Judges from '../components/Judges/Judges';
-import Mentor from '../components/Mentor/Mentor';
-
+import Speaker from "../components/Speaker/Speaker";
+import Judges from "../components/Judges/Judges";
+import Mentor from "../components/Mentor/Mentor";
 
 const HomeWrapper = styled.main`
-	min-height: 100vh;
-	background-color: #09123d;
+  min-height: 100vh;
+  background-color: #040309;
 `;
 
 const Home = () => {
-	return (
-		<HomeWrapper>
-			<Landing />
-			<Judges />
-			<Mentor />
-			<Speaker />
-		</HomeWrapper>
-	);
+  return (
+    <HomeWrapper>
+      <Landing />
+      <Judges />
+      <Speaker />
+      <Mentor />
+    </HomeWrapper>
+  );
 };
 
 export default Home;
