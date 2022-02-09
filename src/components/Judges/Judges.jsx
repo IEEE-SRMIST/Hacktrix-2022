cd0a0fa4c4632f64f651b40f3c80ac7f5d513cad
kevintjinu <52851291+kevintjinu@users.noreply.github.com>
2022-02-09 20:14:43 +0530

    css update

--- src/components/Judges/Judges.jsx
+++ src/components/Judges/Judges.jsx
@@ -2,65 +2,64 @@ import React from "react";
 
 import "../Judges/judges.css";
 import styled from "styled-components";
-import bg from "../../assets/overlay.png";
 
 const JUDGES = [
-  {
-    name: "Steve Wozniak",
-    image: "./img/speaker/judge1.svg",
-    designation: "CEO",
-    company: "echoAR",
-    link: "https://www.linkedin.com/in/alongrinshpoon/",
-  },
-  {
-    name: "Steve Wozniak",
-    image: "./img/speaker/judge1.svg",
-    designation: "CEO",
-    company: "echoAR",
-    link: "https://www.linkedin.com/in/alongrinshpoon/",
-  },
-  {
-    name: "Steve Wozniak",
-    image: "./img/speaker/judge1.svg",
-    designation: "CEO",
-    company: "echoAR",
-    link: "https://www.linkedin.com/in/alongrinshpoon/",
-  },
-  {
-    name: "Steve Wozniak",
-    image: "./img/speaker/judge1.svg",
-    designation: "CEO",
-    company: "echoAR",
-    link: "https://www.linkedin.com/in/alongrinshpoon/",
-  },
-  {
-    name: "Steve Wozniak",
-    image: "./img/speaker/judge1.svg",
-    designation: "CEO",
-    company: "echoAR",
-    link: "https://www.linkedin.com/in/alongrinshpoon/",
-  },
-  {
-    name: "Steve Wozniak",
-    image: "./img/speaker/judge1.svg",
-    designation: "CEO",
-    company: "echoAR",
-    link: "https://www.linkedin.com/in/alongrinshpoon/",
-  },
-  {
-    name: "Steve Wozniak",
-    image: "./img/speaker/judge1.svg",
-    designation: "CEO",
-    company: "echoAR",
-    link: "https://www.linkedin.com/in/alongrinshpoon/",
-  },
-  {
-    name: "Steve Wozniak",
-    image: "./img/speaker/judge1.svg",
-    designation: "CEO",
-    company: "echoAR",
-    link: "https://www.linkedin.com/in/alongrinshpoon/",
-  },
+    {
+        name: "Steve Wozniak",
+        image: "./img/speaker/judge1.svg",
+        designation: "CEO",
+        company: "echoAR",
+        link: "https://www.linkedin.com/in/alongrinshpoon/",
+    },
+    {
+        name: "Steve Wozniak",
+        image: "./img/speaker/judge1.svg",
+        designation: "CEO",
+        company: "echoAR",
+        link: "https://www.linkedin.com/in/alongrinshpoon/",
+    },
+    {
+        name: "Steve Wozniak",
+        image: "./img/speaker/judge1.svg",
+        designation: "CEO",
+        company: "echoAR",
+        link: "https://www.linkedin.com/in/alongrinshpoon/",
+    },
+    {
+        name: "Steve Wozniak",
+        image: "./img/speaker/judge1.svg",
+        designation: "CEO",
+        company: "echoAR",
+        link: "https://www.linkedin.com/in/alongrinshpoon/",
+    },
+    {
+        name: "Steve Wozniak",
+        image: "./img/speaker/judge1.svg",
+        designation: "CEO",
+        company: "echoAR",
+        link: "https://www.linkedin.com/in/alongrinshpoon/",
+    },
+    {
+        name: "Steve Wozniak",
+        image: "./img/speaker/judge1.svg",
+        designation: "CEO",
+        company: "echoAR",
+        link: "https://www.linkedin.com/in/alongrinshpoon/",
+    },
+    {
+        name: "Steve Wozniak",
+        image: "./img/speaker/judge1.svg",
+        designation: "CEO",
+        company: "echoAR",
+        link: "https://www.linkedin.com/in/alongrinshpoon/",
+    },
+    {
+        name: "Steve Wozniak",
+        image: "./img/speaker/judge1.svg",
+        designation: "CEO",
+        company: "echoAR",
+        link: "https://www.linkedin.com/in/alongrinshpoon/",
+    },
 ];
 
 const Heading = styled.h2`
@@ -73,39 +72,32 @@ const Heading = styled.h2`
   text-decoration-color: 2px solid #deb7f5;
   text-shadow: 0px 0px 50px #b741ff;
 `;
-const myStyle = {
-  backgroundImage: `url(${bg})`,
-  fontSize: "50px",
-  height: "100%",
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-};
 
 const Judges = () => {
-  return (
-    <div style={myStyle}>
-      <Heading>Judges</Heading>
-      <div className="row gx-0 py-5 justify-content-center">
-        {JUDGES.map((item, index) => {
-          return (
-            <div className="col-12 col-lg-3 mb-15 speaker">
-              <div className="speakers">
-                <div className="speaker-img">
-                  <img src={`${item.image}`} alt={`${item.name}`} />
-                </div>
-                <div className="speaker-details ">
-                  <div className="speaker-name">{item.name}</div>
-                  <div className="speaker-desg">
-                    {item.designation}, {item.company}
-                  </div>
-                </div>
-              </div>
+    return (
+        <div class="myStyle">
+            <Heading>Judges</Heading>
+            <div className="row gx-0 py-5 justify-content-center">
+                {JUDGES.map((item, index) => {
+                    return (
+                        <div className="col-12 col-lg-3 mb-15 speaker">
+                            <div className="speakers">
+                                <div className="speaker-img">
+                                    <img src={`${item.image}`} alt={`${item.name}`} />
+                                </div>
+                                <div className="speaker-details ">
+                                    <div className="speaker-name">{item.name}</div>
+                                    <div className="speaker-desg">
+                                        {item.designation}, {item.company}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Judges;
--- src/components/Mentor/Mentor.jsx
+++ src/components/Mentor/Mentor.jsx
@@ -1,66 +1,66 @@
 import React from "react";
 
-import "../Judges/judges.css";
+import "../Mentor/mentor.css";
 import styled from "styled-components";
-import bg from "../../assets/overlay.png";
+
 
 const MENTOR = [
-{
-name: "Steve Wozniak",
-image: "./img/speaker/judge1.svg",
-designation: "CEO",
-company: "echoAR",
-link: "https://www.linkedin.com/in/alongrinshpoon/",
-},
-{
-name: "Steve Wozniak",
-image: "./img/speaker/judge1.svg",
-designation: "CEO",
-company: "echoAR",
-link: "https://www.linkedin.com/in/alongrinshpoon/",
-},
-{
-name: "Steve Wozniak",
-image: "./img/speaker/judge1.svg",
-designation: "CEO",
-company: "echoAR",
-link: "https://www.linkedin.com/in/alongrinshpoon/",
-},
-{
-name: "Steve Wozniak",
-image: "./img/speaker/judge1.svg",
-designation: "CEO",
-company: "echoAR",
-link: "https://www.linkedin.com/in/alongrinshpoon/",
-},
-{
-name: "Steve Wozniak",
-image: "./img/speaker/judge1.svg",
-designation: "CEO",
-company: "echoAR",
-link: "https://www.linkedin.com/in/alongrinshpoon/",
-},
-{
-name: "Steve Wozniak",
-image: "./img/speaker/judge1.svg",
-designation: "CEO",
-company: "echoAR",
-link: "https://www.linkedin.com/in/alongrinshpoon/",
-},
-{
-name: "Steve Wozniak",
-image: "./img/speaker/judge1.svg",
-designation: "CEO",
-company: "echoAR",
-link: "https://www.linkedin.com/in/alongrinshpoon/",
-},
-{
-name: "Steve Wozniak",
-image: "./img/speaker/judge1.svg",
-designation: "CEO",
-company: "echoAR",
-link: "https://www.linkedin.com/in/alongrinshpoon/",
-},
+  {
+    name: "Steve Wozniak",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Steve Wozniak",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Steve Wozniak",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Steve Wozniak",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Steve Wozniak",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Steve Wozniak",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Steve Wozniak",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
+  {
+    name: "Steve Wozniak",
+    image: "./img/speaker/judge1.svg",
+    designation: "CEO",
+    company: "echoAR",
+    link: "https://www.linkedin.com/in/alongrinshpoon/",
+  },
 ];
 
 const Heading = styled.h2`
@@ -73,39 +73,32 @@ font-weight: 800;
 text-decoration-color: 2px solid #deb7f5;
 text-shadow: 0px 0px 50px #b741ff;
 `;
-const myStyle = {
-backgroundImage: `url(${bg})`,
-height: "100%",
-fontSize: "50px",
-backgroundSize: "cover",
-backgroundRepeat: "no-repeat",
-};
 
 const Mentor = () => {
-return (
-<div style={myStyle}>
-    <Heading>Mentor</Heading>
-    <div className="row py-5 gx-0 justify-content-center">
+  return (
+    <div class="myStyle">
+      <Heading>Mentor</Heading>
+      <div className="row py-5 gx-0 justify-content-center">
         {MENTOR.map((item, index) => {
-        return (
-        <div className="col-12 col-lg-3 mb-15 speaker">
-            <div className="speakers">
+          return (
+            <div className="col-12 col-lg-3 mb-15 speaker">
+              <div className="speakers">
                 <div className="speaker-img">
-                    <img src={`${item.image}`} alt={`${item.name}`} />
+                  <img src={`${item.image}`} alt={`${item.name}`} />
                 </div>
                 <div className="speaker-details ">
-                    <div className="speaker-name">{item.name}</div>
-                    <div className="speaker-desg">
-                        {item.designation}, {item.company}
-                    </div>
+                  <div className="speaker-name">{item.name}</div>
+                  <div className="speaker-desg">
+                    {item.designation}, {item.company}
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
-        );
+          );
         })}
+      </div>
     </div>
-</div>
-);
+  );
 };
 
 export default Mentor;
