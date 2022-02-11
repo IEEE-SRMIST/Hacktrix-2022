00a3af2f9c54524324ce801c89254dce0fb4e6aa
codebuilder2022 <kumarankaushik@gmail.com>
2022-02-11 18:10:42 +0530

    feat: aboutFinal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Landing from "../components/Home/Landing";
+import About from "../components/About/About";
 import styled from "styled-components";
 
 const HomeWrapper = styled.main`
-	min-height: 100vh;
-	background-color: #09123d;
+  min-height: 100vh;
+  background-color: #050308;
 `;
 
 const Home = () => {
-	return (
-		<HomeWrapper>
-			<Landing />
-		</HomeWrapper>
-	);
+  return (
+    <HomeWrapper>
+      {/* <Landing /> */}
+      <About />
+    </HomeWrapper>
+  );
 };
 
 export default Home;
