01382c0a7d277be19bdb497ee0e06fd7f7e5d0c6
codebuilder2022 <kumarankaushik@gmail.com>
2022-02-18 16:24:02 +0530

    fix

--- src/components/Tracks/Tracks.jsx
+++ src/components/Tracks/Tracks.jsx
@@ -1,91 +1,133 @@
-import React from "react";
-import "./track.css";
-import Slide from "react-reveal/Slide";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import IeeeLogo from "../../assets/img/ieeeLogo.png";
+import ApplyWithDevfolioBtn from "../../assets/img/applyDevfolioBtn.png";
 
-const Tracks = () => {
-  const track_list = [
-    {
-      src: "./img/icons/healthcare.svg",
-      title: "Healthcare",
-      content:
-        "Usher the world of medicine and healthcare forward with curious and inspired solutions.",
-    },
-    {
-      src: "./img/icons/edtech.svg",
-      title: "EdTech",
-      content:
-        "Help the field of education move from the dark ages to the golden age of technology.",
-    },
-    {
-      src: "./img/icons/sustainability.svg",
-      title: "Sustainability",
-      content: "Find innovative solutions to bring relief to our planet Earth.",
-    },
-    {
-      src: "./img/icons/fintech.svg",
-      title: "FinTech",
-      content:
-        "Bring change to transactions and cryptocurrency with promising Blockchain and Financial solutions.",
-    },
-    {
-      src: "./img/icons/arvr.svg",
-      title: "AR/VR",
-      content:
-        "Bring the world of virtual reality closer to actual reality with out-of-the-box solutions.",
-    },
-    {
-      src: "./img/icons/robotics.svg",
-      title: "Robotics",
-      content:
-        "Connecting billions of devices with inspired solutions to the world of IoT.",
-    },
-    {
-      src: "./img/icons/iot.svg",
-      title: "IOT",
-      content:
-        "Connecting billions of devices with inspired solutions to the world of IoT.",
-    },
-    {
-      src: "./img/icons/open.svg",
-      title: "The Matrix",
-      content:
-        "In this Open Track, Let loose your imagination and come up with a unique solution to make the world a better place.",
-    },
+const NavbarComponent = styled.nav`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  padding: 1em 1.5em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 100;
+  @media (max-width: 800px) {
+    position: static;
+  }
+`;
+
+const NavbarList = styled.ul`
+  list-style: none;
+  display: flex;
+  height: 60px;
+  justify-content: space-evenly;
+  gap: 2em;
+  align-items: center;
+  transition: all 500ms;
+  font-size: 1.35rem;
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border-radius: 8px;
+  z-index: 100;
+  @media (max-width: 800px) {
+    gap: 20px;
+    justify-content: space-evenly;
+    position: fixed;
+    height: 40px;
+    padding: 1em 0;
+    bottom: 4rem;
+    left: 50%;
+    font-size: 1rem;
+    transform: translateX(-50%);
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    border-radius: 8px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+    z-index: 100;
+    @media (max-width: 425px) {
+      gap: 10px;
+      left: 50%;
+    }
+  }
+`;
+
+const NavbarItem = styled.li``;
+
+const NavbarLink = styled.a`
+  text-decoration: none;
+  color: #d898ff;
+  text-shadow: 0px 0px 24px #b741ff;
+  :hover {
+    color: #cb7efb;
+    text-shadow: 0px 0px 24px #ab24ff;
+  }
+`;
+
+const LogoImage = styled.a`
+  width: clamp(80px, 100%, 160px);
+  img {
+    width: 100%;
+    height: auto;
+  }
+  @media (max-width: 600px) {
+    width: clamp(60px, 100%, 120px);
+  }
+`;
+
+const DevfolioImage = styled.a`
+  width: clamp(100px, 100%, 240px);
+  img {
+    width: 100%;
+    height: auto;
+  }
+  @media (max-width: 600px) {
+    width: clamp(80px, 100%, 160px);
+  }
+`;
+
+function Navbar() {
+  // const [offset, setOffset] = useState(0);
+  const navLinks = [
+    { path: "#About", name: "HackTrix?" },
+    { path: "#tracks", name: "Tracks" },
+    { path: "#Sponsors", name: "Sponsors" },
+    { path: "#Prizes", name: "Prizes" },
+    { path: "#faqs", name: "FAQs" },
   ];
 
-  return (
-    <div className="track" id="tracks">
-      <div className="container">
-        <div className="track-title">Tracks</div>
-        <div className="tracks">
-          <div className="row justify-content-evenly w-100">
-            {track_list.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className="col-12 col-md-5 col-lg-4 text-center px-3 py-1"
-                >
-                  <div className="track-item">
-                    <div className="parent">
-                      <img
-                        alt="HackTrix"
-                        src={item.src}
-                        className="track-img"
-                      />
-                    </div>
-                    <div className="item-title">
-                      <h2 className="tracks-title">{item.title}</h2>
-                    </div>
-                    <p className="track-content">{item.content}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+  // useEffect(() => {
+  //   const onScroll = () => setOffset(window.pageYOffset);
+  //   window.removeEventListener("scroll", onScroll);
+  //   window.addEventListener("scroll", onScroll, { passive: true });
+  //   return () => window.removeEventListener("scroll", onScroll);
+  // }, []);
 
-export default Tracks;
+  return (
+    <NavbarComponent>
+      <LogoImage href="#Home">
+        <img src={IeeeLogo} alt="IEEE Logo" />
+      </LogoImage>
+      <NavbarList
+        style={
+          {
+            // bottom: offset > 100 ? '1.5em' : '100px',
+          }
+        }
+      >
+        {navLinks.map(({ path, name }) => (
+          <NavbarItem key={name}>
+            <NavbarLink href={`${path}`}>{name}</NavbarLink>
+          </NavbarItem>
+        ))}
+      </NavbarList>
+      <DevfolioImage href="#">
+        <img src={ApplyWithDevfolioBtn} alt="Apply With Devfolio" />
+      </DevfolioImage>
+    </NavbarComponent>
+  );
+}
+
+export default Navbar;
