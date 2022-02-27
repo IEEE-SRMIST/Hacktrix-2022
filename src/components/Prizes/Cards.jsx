419d5af85bec4a62751beb5fed3f29d21194457e
kunalkeshan <kunalkeshan12@gmail.com>
2022-02-27 20:26:18 +0530

    Prize Page Optimization

--- src/components/Prizes/Cards.jsx
+++ src/components/Prizes/Cards.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import LandingBackground from "../../assets/img/landingBg.png";
+
 const CardBox = styled.div`
   width: 100%;
   height: 100%;
   display: grid;
-  grid-template-columns: 350px 350px 350px;
+  grid-template-columns: 1fr 1fr 1fr;
   justify-content: center;
   overflow: none;
+  column-gap: 1rem;
+  font: inherit;
+
   @media (max-width: 768px) {
     display: flex;
     margin-left: 10px;
@@ -17,61 +21,117 @@ const CardBox = styled.div`
     justify-content: center;
   }
 `;
+
+const Title = styled.h1`
+  text-align: center;
+`
+
 const CardContainer = styled.div`
-  min-width: 300px;
-  min-height: 350px;
+  width: clamp(200px, 100%, 300px);
   background: #12151b;
-  box-shadow: 0px 0px 13px rgba(0, 0, 0, 0.5), inset 0px 2px 0px #d7a31a;
+  box-shadow: 0px 0px 13px rgba(0, 0, 0, 0.5), inset 0px 2px 0px #8946A6;
   border-radius: 8px;
   margin: 50px auto;
+  padding: .5em 1em;
+  font: inherit;
+  height: fit-content;
+
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
   @media (max-width: 768px) {
     margin: 20px auto;
     min-width: 250px;
-    min-height: 250px;
   }
 `;
-const TextHolder = styled.p`
-  padding: 10px;
-  margin: 20px auto;
-`;
+
 const Heading = styled.h2`
-  padding: 10% 0px 0px 0px;
-  text-align: center;
+  cursor: pointer;
+  font-size: 1rem;
   color: #ffffff;
   text-shadow: 0px 0px 12px #9226ff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  * {
+    font: inherit;
+  }
 `;
+
+const CardBody = styled.div`
+  font: inherit;
+  display: none;
+`;
+
+const CardBodyList = styled.ul`
+  font: inherit;
+`;
+
+const CardBodyItem = styled.li`
+  font: inherit;
+`;
+
+const Card = ({ heading, body }) => {
+  const [active, setActive] = useState(false);
+
+  return (<CardContainer>
+    <Heading
+      onClick={() => setActive(!active)}><span>{heading}</span> <span>{active ? '-' : '+'}</span></Heading>
+    <CardBody
+      style={{
+        display: active ? 'block' : 'none',
+      }}
+    >
+      <CardBodyList>
+        {
+          body.map((item, index) => (
+            <CardBodyItem key={index}>{item}</CardBodyItem>
+          ))
+        }
+      </CardBodyList>
+    </CardBody>
+  </CardContainer>)
+}
+
+
 const Cards = () => {
+
+  const WINNER_PRIZES = [
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+  ];
+
+  const PARTICIPATION_PRIZES = [
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+    { heading: 'Heading', body: ['prize information', 'prize information', 'prize information'] },
+  ];
+
   return (
     <>
       <section
         style={{
-          paddingTop: "20px",
+          padding: "20px",
           marginTop: "0px",
           backgroundImage: `url(${LandingBackground})`,
           backgroundRepeat: "no-repeat",
           backgroundAttachment: "fixed",
         }}
       >
-        <Heading>Winner Prizes</Heading>
+        <Title>Winner Prizes</Title>
         <CardBox>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
+          {
+            WINNER_PRIZES.map((prize, index) => (
+              <Card {...prize} key={index} />
+            ))
+          }
         </CardBox>
       </section>
       {/* participation starts */}
@@ -85,26 +145,13 @@ const Cards = () => {
           backgroundSize: "cover",
         }}
       >
-        <Heading>Participation Prizes</Heading>
+        <Title>Participation Prizes</Title>
         <CardBox>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
-          <CardContainer>
-            <TextHolder>Hi</TextHolder>
-          </CardContainer>
+          {
+            PARTICIPATION_PRIZES.map((prize, index) => (
+              <Card {...prize} key={index} />
+            ))
+          }
         </CardBox>
       </section>
     </>
