6850a08a364ccbfe4deb788605b9dd8bbcfc7054
legendslayer01 <ajay.satishpriya@gmail.com>
2022-03-09 23:06:01 +0530

    added icon and button

--- src/components/Home/BelowCountDown.jsx
+++ src/components/Home/BelowCountDown.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import DiscordIcon from '../../assets/svg/discord.svg';
+import DiscordIcon from '../../assets/svg/Discord-Logo-White.svg';
 import styled from 'styled-components';
 
 const Atag = styled.a`
   text-decoration: none;
   margin-top: 10px;
+  color: white;
+  font-weight: 500;
+  font-size: 16px;
+`;
+const IMG = styled.img`
+  padding: 5px;
+`;
+const Button = styled.button`
+  display: block;
+  width: fit-content;
+  margin: 10px auto;
+  font-size: 14pt;
+  font-weight: 500;
+  text-decoration: none;
+  border-radius: 0.5rem;
+  background-color: ${({ theme }) => theme.lightPink};
+  transition: all 200ms ease;
+  text-shadow: 0 0 12px ${({ theme }) => theme.glowPink};
+  border: 3px solid ${({ theme }) => theme.lightPink};
+  box-shadow: 0 0 12px ${({ theme }) => theme.glowPink},
+    inset 0 0 12px ${({ theme }) => theme.glowPink};
+`;
+const Span = styled.span`
+  color: white;
 `;
 
 const BelowCountDown = () => {
   return (
-    <Atag href="https://discord.gg/cQwSKeFsQA" target="_blank">
-      <img src={DiscordIcon} alt="discordicon" height="30" width="30"></img>
-    </Atag>
+    <Button>
+      <Atag href="https://discord.gg/cQwSKeFsQA" target="_blank">
+        <IMG src={DiscordIcon} alt="discordicon" height="40" width="40"></IMG>
+        <Span>Join Hacktrix</Span>
+      </Atag>
+    </Button>
   );
 };
 
