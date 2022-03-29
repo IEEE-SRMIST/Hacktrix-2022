4a4afca85fbee0a2647a031487422721a70dbbdf
Snehil <snehilwebber@gmail.com>
2022-03-30 01:07:19 +0530

    Match to designs

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -23,27 +23,76 @@ const People = ({ title, data }) => {
 				<P>Coming up real soon.</P>
 			) : (
 				<div className="row py-5 justify-content-center">
-				{data.map((item, index) => {
-					return (
-						
-						<div key={index} className="col-12 col-lg-4 mb-5 speaker">
-							<div className=" speakers">
-							    <a href={item.link} target="_blank">
-								    <div className="speaker-img ">
-								    	<img src={`${item.image}`} alt={`${item.name}`} />
-								    </div>
-								    <div className="speaker-details ">
-									    <div className="speaker-name">{item.name}</div>
-									    <div className="speaker-desg">
-									    	{item.designation}, {item.company}
-									    </div>
-								    </div>
-							    </a>
+					{data.map((item, index) => {
+						return (
+							<div key={index} className="col-12 col-lg-4 speaker">
+								<div className=" speakers">
+									<a href={item.link} target="_blank" rel="noreferrer">
+										<div className="speaker-img ">
+											<img src={`${item.image}`} alt={`${item.name}`} />
+											<svg
+												width="58"
+												height="58"
+												viewBox="0 0 58 58"
+												fill="none"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<g filter="url(#filter0_d_1210_1169)">
+													<path d="M38 38V20L20 38H38Z" fill="white" />
+												</g>
+												<defs>
+													<filter
+														id="filter0_d_1210_1169"
+														x="0"
+														y="0"
+														width="58"
+														height="58"
+														filterUnits="userSpaceOnUse"
+														color-interpolation-filters="sRGB"
+													>
+														<feFlood
+															flood-opacity="0"
+															result="BackgroundImageFix"
+														/>
+														<feColorMatrix
+															in="SourceAlpha"
+															type="matrix"
+															values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+															result="hardAlpha"
+														/>
+														<feOffset />
+														<feGaussianBlur stdDeviation="10" />
+														<feComposite in2="hardAlpha" operator="out" />
+														<feColorMatrix
+															type="matrix"
+															values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0"
+														/>
+														<feBlend
+															mode="normal"
+															in2="BackgroundImageFix"
+															result="effect1_dropShadow_1210_1169"
+														/>
+														<feBlend
+															mode="normal"
+															in="SourceGraphic"
+															in2="effect1_dropShadow_1210_1169"
+															result="shape"
+														/>
+													</filter>
+												</defs>
+											</svg>
+										</div>
+										<div className="speaker-details ">
+											<div className="speaker-name">{item.name}</div>
+											<div className="speaker-desg">
+												{item.designation}, {item.company}
+											</div>
+										</div>
+									</a>
+								</div>
 							</div>
-						</div>
-						
-					);
-				})}
+						);
+					})}
 				</div>
 			)}
 		</PeopleSection>
